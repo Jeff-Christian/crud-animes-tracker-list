279b8089e49bb318e4fe0fac2ba389aadc86f344
Jeff Christian <seu-email@exemplo.com>
2025-04-09 22:59:11 -0300

    editing navbar with buttons to add new animes

---
+++ FRONTEND/animeTrackerCrud/src/Component/NavBar.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Anime list</h1>
+      </div>
+      <nav>
+        <img
+          src="./assets/Images/LOGO BRANCO.png"
+          alt="Logo animes tracker"
+          id="logo"
+        />
+        <button id="add">Adicionar Novo</button>
+        <button id="login">Login</button>
+      </nav>
+    </>
+  );
+}
+
+export default App;
