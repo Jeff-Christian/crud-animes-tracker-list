03ffd58db01f3bac77c4eb9ec6fee1b1299021b0
Jeff Christian <seu-email@exemplo.com>
2025-04-10 17:57:27 -0300

    first apparence for cards with a form

--- FRONTEND/animeTrackerCrud/src/Component/card.jsx
+++ FRONTEND/animeTrackerCrud/src/Component/card.jsx
@@ -3,7 +3,17 @@ import "../assets/CSS/Cards.css";
 function Card() {
   return (
     <>
-      <div className="cards">cards forms</div>
+      <div className="cards">
+        <form action="">
+          <input type="text" placeholder="Nome do Anime" />
+          <input type="text" placeholder="Onde Assistiu?" />
+          <input type="text" placeholder="Nota pro Anime" />
+          <input type="text" placeholder="Data" />
+          <button type="submit" id="publish">
+            Publicar
+          </button>
+        </form>
+      </div>
     </>
   );
 }
