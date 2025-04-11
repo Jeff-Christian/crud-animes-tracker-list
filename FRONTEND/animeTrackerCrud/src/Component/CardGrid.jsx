9d9b550aa40319ef24ef5439b41751000c291058
Jeff Christian <seu-email@exemplo.com>
2025-04-11 20:21:53 -0300

    design of cards and receveing datas of backend, insert of buttons edit and trash

--- FRONTEND/animeTrackerCrud/src/Component/CardGrid.jsx
+++ FRONTEND/animeTrackerCrud/src/Component/CardGrid.jsx
@@ -1,18 +1,29 @@
 //import axios from "axios";
 
-import Card from "./card";
-
 function Grid({ animes }) {
   return (
-    <div className="cards">
+    <div id="bodyCards">
       {animes.map((anime, i) => (
-        <div key={i}>
-          <p>
-            {anime.AnimeName} {/* Display the title of the anime */}
-            {anime.where}
-            {anime.Rating} {/* Display the type of the anime */}
-            {anime.Date} {/* Display the type of the anime */}
-          </p>
+        <div className="cards" key={i}>
+          <div className="image">
+            <div className="changes">
+              <button>editar</button>
+              <button>excluir</button>
+            </div>
+          </div>
+          <div>
+            <h3>
+              Nome do anime: {anime.AnimeName}
+              {/* Display the title of the anime */}
+            </h3>
+            <h3>Assisti na: {anime.where}</h3>
+            <h3>
+              Nota: {anime.Rating} {/* Display the type of the anime */}
+            </h3>
+            <h3>
+              Data: {anime.Date} {/* Display the type of the anime */}
+            </h3>
+          </div>
         </div> // Pass the anime object as a prop to Card
       ))}
     </div>
