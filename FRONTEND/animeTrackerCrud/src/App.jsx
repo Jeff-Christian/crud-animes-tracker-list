4d0d5daa98cf7f1dd2551e8199eaabd9dea89daa
Jeff Christian <seu-email@exemplo.com>
2025-04-12 14:56:48 -0300

    Inserting all methods about add/post new anime to list

--- FRONTEND/animeTrackerCrud/src/App.jsx
+++ FRONTEND/animeTrackerCrud/src/App.jsx
@@ -5,6 +5,8 @@ import Grid from "./Component/CardGrid.jsx";
 
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [animes, setAnimes] = useState([]);
@@ -15,7 +17,7 @@ function App() {
       const res = await axios.get("http://localhost:8800");
       setAnimes(res.data.sort((a, b) => (a.AnimeName > b.AnimeName ? 1 : -1)));
     } catch (error) {
-      console.error("Error fetching data:", error);
+      toast.error("Error fetching data:", error);
     }
   };
 
@@ -29,6 +31,7 @@ function App() {
       <div id="bodyCards">
         <Card></Card>
         <Grid animes={animes}></Grid>
+        <ToastContainer autoClose={3000} position={toast} />
       </div>
     </>
   );
--- FRONTEND/animeTrackerCrud/src/Component/card.jsx
+++ FRONTEND/animeTrackerCrud/src/Component/card.jsx
@@ -1,12 +1,69 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 import "../assets/CSS/Cards.css";
 
-function Card() {
+function Card({ getAnimes, onEdit, setOnEdit }) {
   const ref = useRef();
+
+  useEffect(() => {
+    if (onEdit) {
+      const anime = ref.current;
+      anime.AnimeName.value = onEdit.AnimeName;
+      anime.where.value = onEdit.where;
+      anime.Rating.value = onEdit.Rating;
+      anime.Date.value = onEdit.Date;
+    }
+  }, [onEdit]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const anime = ref.current;
+
+    if (
+      !anime.AnimeName.value ||
+      !anime.where.value ||
+      !anime.Rating.value ||
+      !anime.Date.value
+    ) {
+      return alert("Preencha todos os campos!");
+    }
+
+    if (onEdit) {
+      await axios
+        .put("http://localhost:8800/" + onEdit.id, {
+          AnimeName: anime.AnimeName.value,
+          where: anime.where.value,
+          Rating: anime.Rating.value,
+          Date: anime.Date.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    } else {
+      await axios
+        .post("http://localhost:8800/", {
+          AnimeName: anime.AnimeName.value,
+          where: anime.where.value,
+          Rating: anime.Rating.value,
+          Date: anime.Date.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    }
+
+    anime.AnimeName.value = "";
+    anime.where.value = "";
+    anime.Rating.value = "";
+    anime.Date.value = "";
+    // Limpa os campos após a inserção ou atualização
+    setOnEdit(null);
+    // Atualiza a lista de animes após a inserção ou atualização
+    getAnimes();
+  };
 
   return (
     <div className="cards">
-      <form ref={ref} action="">
+      <form ref={ref} onSubmit={handleSubmit} action="">
         <input type="text" name="AnimeName" placeholder="Nome do Anime" />
         <input type="text" name="where" placeholder="Onde Assistiu?" />
         <input type="text" name="Rating" placeholder="Nota pro Anime" />
