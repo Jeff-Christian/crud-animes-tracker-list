b754f31408b9e7bcb6a65d25ca82ab7665976302
Jefferson Christian <jeffchristian.webdesigner@gmail.com>
2025-04-18 23:34:21 -0300

    creating register page frontend

--- FRONTEND/animeTrackerCrud/src/Component/Routes/Register.jsx
+++ FRONTEND/animeTrackerCrud/src/Component/Routes/Register.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "../CSS/Register.css";
+import logo from "../../assets/Images/LOGO BRANCO.png";
+import Login from "./Login";
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <>
+      <div className="containerRegister">
+        <img src={logo} alt="logo anime Tracker List" className="logo" />
+        <form action="" className="formRegister">
+          <p>Criar uma nova conta</p>
+          <input type="text" name="name" placeholder="Nome" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <input type="password" name="senha" placeholder="Senha" required />
+          <p>
+            Ao clicar em Cadastre-se, você concorda com nosso Termos, Política
+            de Privacidade e Política de Cookies.
+          </p>
+
+          <button type="submit" className="register">
+            Cadastre-se
+          </button>
+          <a href={<Login />}> Já tem uma conta? </a>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Register;
