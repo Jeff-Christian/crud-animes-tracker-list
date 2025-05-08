fff571e15de8396a61ce3b85d589ccdacf98a2df
Jefferson Christian <jeffchristian.webdesigner@gmail.com>
2025-05-08 20:33:50 -0300

    creating enviroment variables and connection with database in railway

--- API/db.js
+++ API/db.js
@@ -1,8 +1,9 @@
 import mysql from "mysql";
+require("dotenv").config();
+// Create a connection to the database
 
-export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "AmethystSQL26",
-  database: "animecrud",
-});
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQL_DATABASE}`;
+
+export const db = mysql.createConnection(urlDB);
+
+module.exports = db; // Export the connection object
