4cfc7bbcb4de3cbacffe790232b20f084735bb02
Jefferson Christian <jeffchristian.webdesigner@gmail.com>
2025-05-09 19:20:09 -0300

    fix: add Pool for mysql connection

--- API/db.js
+++ API/db.js
@@ -5,6 +5,6 @@ dotenv.config(); // Load environment variables from .env file
 
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQL_DATABASE}`;
 
-export const db = mysql.createConnection(urlDB);
+export const db = mysql.createPool(urlDB);
 
 //module.exports = db; // Export the connection object
