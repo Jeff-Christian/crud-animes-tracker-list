e90fc82bb07b376e474ee1baa4e1cb8e969cbc7c
Jefferson Christian <jeffchristian.webdesigner@gmail.com>
2025-05-08 21:35:50 -0300

    import dotenv in db.js

--- API/db.js
+++ API/db.js
@@ -1,6 +1,7 @@
 import mysql from "mysql";
-require("dotenv").config();
-// Create a connection to the database
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env file
 
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQL_DATABASE}`;
 
